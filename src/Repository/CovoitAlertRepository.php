c18dd7461114bd244b95aa95c3a090b4a5c6a95a
Valentin Koeltgen <47000714+koeltv@users.noreply.github.com>
2021-04-20 15:46:49 +0200

    :sparkles: (Entity\CovoitAlert) Create CovoitAlert entity and seeder

---
+++ src/Repository/CovoitAlertRepository.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\CovoitAlert;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @method CovoitAlert|null find($id, $lockMode = null, $lockVersion = null)
+ * @method CovoitAlert|null findOneBy(array $criteria, array $orderBy = null)
+ * @method CovoitAlert[]    findAll()
+ * @method CovoitAlert[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class CovoitAlertRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, CovoitAlert::class);
+    }
+
+    // /**
+    //  * @return CovoitAlert[] Returns an array of CovoitAlert objects
+    //  */
+    /*
+    public function findByExampleField($value)
+    {
+        return $this->createQueryBuilder('c')
+            ->andWhere('c.exampleField = :val')
+            ->setParameter('val', $value)
+            ->orderBy('c.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+    */
+
+    /*
+    public function findOneBySomeField($value): ?CovoitAlert
+    {
+        return $this->createQueryBuilder('c')
+            ->andWhere('c.exampleField = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+    */
+}
