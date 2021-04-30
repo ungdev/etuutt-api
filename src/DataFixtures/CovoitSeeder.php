ec7b8ba6398c8952e205a7650b56df907249344f
Valentin Koeltgen <47000714+koeltv@users.noreply.github.com>
2021-04-30 16:08:37 +0200

    :bug: (DataFixtures) Merged all covoits related seeders into one

--- src/DataFixtures/CovoitSeeder.php
+++ src/DataFixtures/CovoitSeeder.php
@@ -3,6 +3,8 @@
 namespace App\DataFixtures;
 
 use App\Entity\Covoit;
+use App\Entity\CovoitAlert;
+use App\Entity\CovoitMessage;
 use App\Entity\User;
 use DateTime;
 use Doctrine\Bundle\FixturesBundle\Fixture;
@@ -82,6 +84,82 @@ class CovoitSeeder extends Fixture implements DependentFixtureInterface
 
             //On persiste le covoit dans la base de données
             $manager->persist($covoit);
+        }
+        $manager->flush();
+
+        //Récupération des users
+        $userRepository = $manager->getRepository(User::class);
+        $users = $userRepository->findAll();
+
+        //Création de 30 covoitAlerts
+        for ($i = 0; $i < 30; ++$i) {
+            //Créations d'une covoitAlert
+            $covoitAlert = new CovoitAlert();
+
+            //On lie l'alerte à un user
+            $covoitAlert->setUser($faker->randomElement($users));
+
+            $covoitAlert->setPriceMax($faker->numberBetween(10, 25));
+
+            //Création des timestamps
+            $covoitAlert->setCreatedAt($faker->dateTimeThisYear);
+            $days = (new DateTime())->diff($covoitAlert->getCreatedAt())->days;
+            $covoitAlert->setUpdatedAt($faker->dateTimeBetween('-'.$days.' days'));
+
+            $days = (new DateTime())->diff($covoitAlert->getUpdatedAt())->days;
+            $covoitAlert->setStartDate($faker->dateTimeBetween('-'.$days.' days'));
+
+            $days = (new DateTime())->diff($covoitAlert->getStartDate())->days;
+            $covoitAlert->setEndDate($faker->dateTimeBetween('-'.$days.' days'));
+
+            //Création des villes de départ et d'arrivée
+            $covoitAlert->setStartCity($faker->city);
+
+            $covoitAlert->setEndCity($faker->city);
+
+            //On persiste l'alerte dans la base de données
+            $manager->persist($covoitAlert);
+        }
+        $manager->flush();
+
+        //Récupération des covoits et des users
+        $covoitRepository = $manager->getRepository(Covoit::class);
+        $covoits = $covoitRepository->findAll();
+        $userRepository = $manager->getRepository(User::class);
+        $users = $userRepository->findAll();
+
+        //Création de 100 covoitMessages
+        for ($i = 0; $i < 100; ++$i) {
+            //Créations d'un covoitMessage
+            $covoitMessage = new CovoitMessage();
+
+            //On lie le message à un covoit
+            $covoitMessage->setCovoit($faker->randomElement($covoits));
+
+            //On ajoute un user en tant qu'auteur du covoitMessage
+            $covoitMessage->setAuthor($faker->randomElement($users));
+
+            //Création du texte
+            $text = '';
+            for ($j = 0; $j < 5; ++$j) {
+                $text .= '<p>';
+                $text .= str_repeat($faker->word, 9);
+                $text .= '</p>';
+            }
+            $covoitMessage->setText($text);
+
+            //Création des timestamps
+            $covoitMessage->setCreatedAt($faker->dateTimeBetween('-3 years'));
+            $days = (new DateTime())->diff($covoitMessage->getCreatedAt())->days;
+            $covoitMessage->setUpdatedAt($faker->dateTimeBetween('-'.$days.' days'));
+            //Soft delete aléatoire d'un Timestamps (Avec une chance de 10%)
+            if ($faker->boolean(10)) {
+                $days = (new DateTime())->diff($covoitMessage->getCreatedAt())->days;
+                $covoitMessage->setDeletedAt($faker->dateTimeBetween('-'.$days.' days'));
+            }
+
+            //On persiste le covoitMessage dans la base de données
+            $manager->persist($covoitMessage);
         }
         $manager->flush();
     }
