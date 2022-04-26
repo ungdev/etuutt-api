3c2a16d6f65f0e0478b73f2d7c292412bff28a95
Teddy Roncin <teddy.roncin35@gmail.com>
2022-04-26 16:06:34 +0200

    :white_check_mark: (tests) added tests for route GET /users
    testing content of json

--- tests/UserTest.php
+++ tests/UserTest.php
@@ -3,38 +3,45 @@
 namespace App\Tests;
 
 use ApiPlatform\Core\Bridge\Symfony\Bundle\Test\ApiTestCase;
+use ApiPlatform\Core\Bridge\Symfony\Bundle\Test\Client;
 use App\Repository\UserRepository;
+use Symfony\Component\HttpFoundation\Response;
 
 class UserTest extends ApiTestCase
 {
 
-    public function testSomething(): void
+    public function test_POST_users(): void
     {
-        $client = static::createClient([], [
-            'CAS-LOGIN' => 'abc'
-        ]);
-        $userRepository = static::getContainer()->get(UserRepository::class);
-        $testUser = $userRepository->findAll()[0];
-        $client->loginUser($testUser);
-        $response = $client->request('POST', '/users');
-
-        //$response = $client->request('GET', '/users/');
-
+        $client = static::createConnectedClient();
+        $crawler = $client->request('GET', 'localhost:8000/users');
+        $response = json_decode($crawler->getContent());
         $this->assertResponseIsSuccessful();
-        /*$this->assertMatchesJsonSchema([
-            "type" => "object",
-            "hydra:member" => [
-                "type" => "array",
-                "items" => [
-                    "type" => "object",
-                    "@id" => ["type" => "string"],
-                    "@type" => ["type" => "string"],
-                    "id" => ["type" => "string"],
-                    "login" => ["type" => "string"],
-                    "firstName" => ["type" => "string"],
-                    "lastName" => ["type" => "string"],
-                ]
-            ]
-        ]);*/
+        $this->assertResponseStatusCodeSame(Response::HTTP_OK);
+        $this->assertIsArray($response->{"hydra:member"});
+        $this->assertNotEmpty($response->{"hydra:member"});
+        foreach ($response->{"hydra:member"} as $member) {
+            $this->assertMatchesRegularExpression("/^[\da-f]{8}-[\da-f]{4}-[\da-f]{4}-[\da-f]{4}-[\da-f]{12}$/", $member->{"id"});
+            $this->assertNotEmpty($member->{"login"});
+            $this->assertNotEmpty($member->{"firstName"});
+            $this->assertNotEmpty($member->{"lastName"});
+            $this->assertMatchesRegularExpression("/^https?:\\/\\/[\w.-]*\\.[\w-].+$/", $member->{"infos"}->{"avatar"});
+            $this->assertIsArray($member->{"mailsPhones"});
+        }
+        $this->assertIsNumeric($response->{"hydra:totalItems"});
+        $this->assertTrue($response->{"hydra:totalItems"} >= 0);
+        $matches = array();
+        $this->assertEquals(1, preg_match("/^\\/users\\?page=(?<id>\d+)+$/", $response->{"hydra:view"}->{"@id"}, $matches));
+        $this->assertArrayHasKey("id", $matches);
+        $this->assertEquals(1, preg_match("/^\\/users\\?page=(?<id>\d+)+$/", $response->{"hydra:view"}->{"hydra:next"}, $matches));
+        $this->assertArrayHasKey("id", $matches);
+        $this->assertEquals(1, preg_match("/^\\/users\\?page=(?<id>\d+)+$/", $response->{"hydra:view"}->{"hydra:last"}, $matches));
+        $this->assertArrayHasKey("id", $matches);
+    }
+
+    private static function createConnectedClient(): Client
+    {
+        $client = static::createClient();
+        $client->setDefaultOptions([ 'headers' => [ 'CAS-LOGIN' => 'admin' ]]);
+        return $client;
     }
 }
