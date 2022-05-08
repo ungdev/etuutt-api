ec7b4920a4701506967bd7b0f6269e5aadbb0fc3
Teddy Roncin <teddy.roncin35@gmail.com>
2022-05-08 18:25:57 +0200

    :bug: (User) fixed entity User
    /users routes will now return all fields, even if they are empty (in this case there content will be null)
    
    Closes #28

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -28,6 +28,9 @@ use Symfony\Component\Validator\Constraints as Assert;
             'security' => "is_granted('ROLE_USER')",
             'pagination_items_per_page' => 10,
         ],
+        normalizationContext: [
+            'skip_null_values' => false,
+        ],
         collectionOperations: [
             'get' => [
                 'normalization_context' => [
