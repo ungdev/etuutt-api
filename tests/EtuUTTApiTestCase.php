120a3b46e310f5001fa557eb7d854d3d9da1084b
Teddy Roncin <teddy.roncin35@gmail.com>
2022-05-21 18:58:31 +0200

    :recycle: (tests) 'test' user now saved + directory renamed
    Class EtuUTTApiTestCase now saves the 'test' user, created in the setUp method. Directory tests/users was renamed to
    tests/Users, to be more coherent with conventions used in the project

--- tests/EtuUTTApiTestCase.php
+++ tests/EtuUTTApiTestCase.php
@@ -3,36 +3,28 @@
 namespace App\Tests;
 
 use ApiPlatform\Core\Bridge\Symfony\Bundle\Test\ApiTestCase;
-use App\DataFixtures\UserSeeder;
 use App\Entity\User;
-use App\Entity\UserBan;
-use App\Entity\UserBDEContribution;
-use App\Entity\UserEtuUTTTeam;
-use App\Entity\UserMailsPhones;
-use App\Util\Slug;
-use App\Util\Text;
-use DateTime;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\DataFixtures\Loader;
 use Doctrine\Common\DataFixtures\Purger\ORMPurger;
 use Doctrine\ORM\EntityManager;
-use Faker\Factory;
 
 abstract class EtuUTTApiTestCase extends ApiTestCase
 {
 
     protected EntityManager $em;
+    protected User $user;
 
     protected function setUp(): void
     {
         $this->em = $this->getContainer()->get('doctrine')->getManager();
         (new ORMPurger($this->em))->purge();
-        $user = new User();
-        $user->setFirstName('test');
-        $user->setLastName('test');
-        $user->setLogin('test');
-        $user->addRole('ROLE_ADMIN');
-        $this->em->persist($user);
+        $this->user = new User();
+        $this->user->setFirstName('test');
+        $this->user->setLastName('test');
+        $this->user->setLogin('test');
+        $this->user->addRole('ROLE_ADMIN');
+        $this->em->persist($this->user);
         $this->em->flush();
     }
 
