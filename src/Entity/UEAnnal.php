262a1476d14d820cd7442e77715f095a82ffd4b1
Thomas Ritaine <thomas.ritaine@outlook.com>
2023-02-21 18:28:52 -1000

    Removing typo mistake

--- src/Entity/UEAnnal.php
+++ src/Entity/UEAnnal.php
@@ -6,6 +6,8 @@ use App\Entity\Traits\SoftDeletableTrait;
 use App\Entity\Traits\TimestampsTrait;
 use App\Entity\Traits\UUIDTrait;
 use App\Repository\UEAnnalRepository;
+use DateTime;
+use DateTimeInterface;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
